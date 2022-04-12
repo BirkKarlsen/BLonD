Corrected signs in modulation function.

blond/llrf/signal_processing.py
@@ -99,8 +99,8 @@ def modulator(signal, omega_i, omega_f, T_sampling, phi_0=0):
     # Pre compute sine and cosine for speed up
     cs = np.cos(delta_phi + phi_0)
     sn = np.sin(delta_phi + phi_0)
-    I_new = cs*signal.real + sn*signal.imag
-    Q_new = - sn*signal.real + cs*signal.imag
+    I_new = cs*signal.real - sn*signal.imag
+    Q_new = sn*signal.real + cs*signal.imag
 
     return I_new + 1j*Q_new
 